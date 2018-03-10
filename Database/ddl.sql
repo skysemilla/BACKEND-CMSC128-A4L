7b5e835b0085dc21418003461d2be09ed4804301
Aaron Magnaye <avmagnaye@up.edu.ph>
2018-03-10 15:06:56 +0800

    table updates
    fixed final bugs
    foreign key show as MUL

--- Database/ddl.sql
+++ Database/ddl.sql
@@ -14,7 +14,7 @@ create table FACULTY(
 	department varchar(10) not null,
 	college varchar(20) not null,
 	rank varchar(10) not null,
-	constraint faculty_emp_pk PRIMARY KEY (emp_id)
+	constraint faculty_emp_id_pk PRIMARY KEY (emp_id)
 );
 
 create table FACULTY_NAME(
@@ -34,8 +34,8 @@ create table ACTIVITY(
 	start_time datetime not null,
 	end_time datetime not null,
 	fsr_id int not null,
-	constraint activity_activity_id PRIMARY KEY (activity_id),
-	constraint activity_fsr_id foreign key (fsr_id) references FSR(fsr_id)
+	constraint activity_activity_id_pk PRIMARY KEY (activity_id),
+	constraint activity_fsr_id_fk foreign key (fsr_id) references FSR(fsr_id)
 );
 
 create table SERVICE(
@@ -47,8 +47,8 @@ create table SERVICE(
 	role varchar(10) not null,
 	credits int (10) not null,
 	fsr_id int not null,	
-	constraint service_service_id PRIMARY KEY (service_id),
-	constraint service_fsr_id foreign key (fsr_id) references FSR(fsr_id)
+	constraint service_service_id_pk PRIMARY KEY (service_id),
+	constraint service_fsr_id_fk foreign key (fsr_id) references FSR(fsr_id)
 );
 
 create table PUBLICATION(
@@ -62,18 +62,15 @@ create table PUBLICATION(
 	end_date datetime not null,
 	fsr_id int not null,
 	constraint publication_id_pk PRIMARY key (publication_id),
-	constraint fsr_id_fk foreign key (fsr_id) references FSR(fsr_id)
+	constraint publication_fsr_id_fk foreign key (fsr_id) references FSR(fsr_id)
 );
 
 create table COWORKER(
 	coworker_id int AUTO_INCREMENT,
-	emp_id int not null,
 	publication_id int not null,
 	constraint coworker_coworker_id PRIMARY KEY (coworker_id),
-	constraint coworker_emp_id foreign key (emp_id) references FACULTY(emp_id),
-	constraint coworker_publication_id foreign key (publication_id) references PUBLICATION(publication_id)
+	constraint coworker_publication_id_fk foreign key (publication_id) references PUBLICATION(publication_id)
 );
-
 
 create table CONSULTATION(
 	consultation_id int AUTO_INCREMENT,
@@ -81,43 +78,39 @@ create table CONSULTATION(
 	consultation_date date not null,
 	consultation_place varchar(255) not null,
 	fsr_id int not null,
-	constraint consultation_id_pk PRIMARY key (consultation_id),
-	constraint fsr_id_fk foreign key (fsr_id) references FSR(fsr_id)
+	constraint consultation_consultation_id_pk PRIMARY key (consultation_id),
+	constraint consultation_fsr_id_fk foreign key (fsr_id) references FSR(fsr_id)
 );
 
-create table POSITION(
-	position_id int not null AUTO_INCREMENT PRIMARY key,
-	office varchar(255),
+create table POSITIONN(
+	position_id int AUTO_INCREMENT,
+	office varchar(255) not null,
 	credit_units int not null,
 	fsr_id int not null,
-	emp_id varchar(10) not null,
-	constraint position_id_pk PRIMARY key (position_id),
-	constraint fsr_id_fk foreign key (fsr_id) references FSR(fsr_id),
-	constraint emp_id_fk foreign key (emp_id) references FACULTY(emp_id)
+	constraint position_position_id_pk PRIMARY key (position_id),
+	constraint position_fsr_id_fk foreign key (fsr_id) references FSR(fsr_id)
 );
 
-
-
 create table SUBJECT(
-	subject_code varchar(10) not null PRIMARY key,
+	subject_code varchar(10) not null,
 	room varchar(255) not null,
 	start_time datetime not null,
 	end_time datetime not null,
-	section_code varchar(10) not null unique,
-	fsr_id int not null foreign key,
+	section_code varchar(10) not null,
+	fsr_id int not null,
 	constraint subject_code_pk PRIMARY key (subject_code),
 	constraint section_code_uk unique (section_code),
-	constraint fsr_id_fk foreign key (fsr_id) references FSR(fsr_id)
+	constraint subject_fsr_id_fk foreign key (fsr_id) references FSR(fsr_id)
 );
 
 create table WORKLOAD(
-	subject_id int AUTO_INCREMENT not null PRIMARY key,
+	subject_id int AUTO_INCREMENT not null,
 	no_of_students int not null,
 	constraint subject_id_pk PRIMARY key (subject_id)
 );
 
 create table STUDYLOAD(
-	subject_id int AUTO_INCREMENT not null PRIMARY key,
+	subject_id int AUTO_INCREMENT not null,
 	school varchar(255) not null,
 	constraint subject_id_pk PRIMARY key (subject_id)
 );

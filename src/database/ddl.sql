82f1c283ef91c24fdc7d8270732f0135f707dc39
Aaron Magnaye <avmagnaye@up.edu.ph>
2018-03-18 06:14:26 +0800

    Comments

--- src/database/ddl.sql
+++ src/database/ddl.sql
@@ -22,7 +22,7 @@ create table EMPLOYEE( -- REPRESENTS FACULTY MEMBERS
   constraint employee_username_uk UNIQUE KEY (username)
 );
 
-create table ACTIVITY(
+create table ACTIVITY( -- REPRESENTS ACTIVITIES BY THE FOREIGN KEY EMPLOYEE
   activity_id int AUTO_INCREMENT,
   credit_unit int (255) not null,
   activity_name varchar(20) not null,
@@ -37,7 +37,7 @@ create table ACTIVITY(
   constraint activity_emp_id_fk foreign key (emp_id) references EMPLOYEE(emp_id)
 );
 
-create table SERVICE(
+create table SERVICE( -- REPRESENTS SERVICES BY THE FOREIGN KEY EMPLOYEE
   service_id int not null AUTO_INCREMENT,
   category varchar(255) not null,
   title varchar(255) not null,
@@ -50,7 +50,7 @@ create table SERVICE(
   constraint service_emp_id_fk foreign key (emp_id) references EMPLOYEE(emp_id)
 );
 
-create table PUBLICATION(
+create table PUBLICATION( -- REPRESENTS THE PUBLICATIONS BY THE FOREIGN KEY EMPLOYEE
   publication_id int not null AUTO_INCREMENT,
   credit_units int not null,
   category varchar(255) not null,
@@ -64,7 +64,7 @@ create table PUBLICATION(
   constraint publication_emp_id_fk foreign key (emp_id) references EMPLOYEE(emp_id)
 );
 
-create table COWORKER(
+create table COWORKER( -- REPRESENTS A COWORKER PRESENT IN A PUBLICATION
   coworker_id int AUTO_INCREMENT,
   emp_id varchar(10) not null, 
   publication_id int not null,
@@ -73,7 +73,7 @@ create table COWORKER(
   constraint coworker_emp_id_fk foreign key (emp_id) references EMPLOYEE(emp_id)
 );
 
-create table CONSULTATION(
+create table CONSULTATION( -- REPRESENTS CONSULTATION HOURS
   consultation_id int AUTO_INCREMENT,
   consultation_start_time datetime not null,
   consultation_end_time datetime not null,
@@ -83,13 +83,13 @@ create table CONSULTATION(
   constraint consultation_emp_id_fk foreign key (emp_id) references EMPLOYEE(emp_id)
 );
 
-create table CONSULTATION_DAY(
+create table CONSULTATION_DAY( -- MULTI-VALUED ATTRIBUTE OF CONSULTATION
   consultation_id int not null,
   day varchar(255) not null,
   constraint consultation_day_consultation_id_fk foreign key (consultation_id) references CONSULTATION(consultation_id)
 );
 
-create table POSITIONN(
+create table POSITIONN( -- REPRESENTS THE POSITIONS OBTAINED BY THE USER | pardon for the double n, apparently position is a reserved word
   position_id int AUTO_INCREMENT,
   office varchar(255) not null,
   credit_units int not null,
@@ -109,7 +109,7 @@ create table SUBJECT( -- RESURRECTED SUBJECT TABLE FOR TEACHINGLOAD AND STUDYLOA
   constraint subject_subject_code_pk PRIMARY key (subject_code)
 );
 
-create table SUBJECT_DAY(
+create table SUBJECT_DAY( -- REPRESENTS THE SUBJECTS OF A USER
   day varchar(255) not null,
   subject_code varchar(255) not null,
   constraint subject_day_subject_code_fk foreign key (subject_code) references SUBJECT(subject_code)
@@ -137,6 +137,29 @@ create table STUDYLOAD( -- SAME CONCEPT AS THE TEACHINGLOAD
   constraint studyload_emp_id_fk foreign key (emp_id) references EMPLOYEE(emp_id),
   constraint studyload_subject_code_fk foreign key (subject_code) references SUBJECT(subject_code)
 );
+
+---- PROCEDURES FOR EMPLOYEE
+
+DROP PROCEDURE IF EXISTS view_employee; 
+DELIMITER GO
+CREATE PROCEDURE view_employee()
+  BEGIN 
+    SELECT * from EMPLOYEE;
+END;
+GO
+DELIMITER ;
+
+DROP PROCEDURE IF EXISTS view_employee_by_ID; 
+DELIMITER GO
+CREATE PROCEDURE view_employee_by_ID(emp_id_view int)
+  BEGIN 
+    SELECT * from EMPLOYEE
+    where emp_id = emp_id_view;
+END;
+GO
+DELIMITER ;
+
+---- END OF PROCEDURES FOR EMPLOYEE
 
 ---- PROCEDURES FOR ACTIVITY
 
@@ -207,7 +230,9 @@ END;
 GO
 DELIMITER ;
 
+---- END OF PROCEDURES FOR ACTIVITY
 
+---- PROCEDURES FOR POSITIONN
 
 DROP PROCEDURE IF EXISTS view_position; 
 DELIMITER GO
@@ -255,6 +280,10 @@ CREATE PROCEDURE update_position(position_id_update int,
 END;
 GO
 DELIMITER ;
+
+---- END OF PROCEDURES FOR POSITIONN
+
+---- PROCEDURES FOR SERVICE
 
 DROP PROCEDURE IF EXISTS view_service; 
 DELIMITER GO
@@ -325,6 +354,8 @@ CREATE PROCEDURE update_service( service_id_u int,
 END;
 GO
 DELIMITER ;
+
+---- END OF PROCEDURES FOR SERVICE
 
 INSERT INTO `EMPLOYEE` VALUES ('0000000000', 'admin','admin','ADMIN', 'hello', 'world', '!', 'ADMIN', 'ICS', 'CAS');
 INSERT INTO `EMPLOYEE` VALUES ('0000000001', 'bea', 'bautista123', 'USER', 'Bianca', 'B?', 'Bautista', 'FACULTY', 'ICS', 'CAS');

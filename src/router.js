2878588e8648590c4ace27ee478d86208f17d657
iaurriza <iaurriza@up.edu.ph>
2018-04-11 15:02:53 +0800

    integrated study credentials

--- src/router.js
+++ src/router.js
@@ -2,11 +2,11 @@ import { Router } from 'express';
 
 // put import routers here
 import authRouter from './entities/auth/router';
-import extensionRouter from './entities/extensions/router';
+import extensionRouter from './entities/extension/router';
 import publicationsRouter from './entities/publications/router';
 import signupRouter from './entities/signup/router';
-// import studyRouter from './entities/studyload/router';
-// import teachRouter from './entities/teachload/router';
+import studyRouter from './entities/studyload/router';
+import teachRouter from './entities/teachload/router';
 import facultyRouter from './entities/facultylist/router';
 // import fsrRouter from './entities/fsrlist/router';
 import adminWorkRouter from './entities/adminWork/router';
@@ -21,8 +21,8 @@ router.use('/', authRouter);
 router.use(extensionRouter);
 router.use(publicationsRouter);
 router.use(signupRouter);
-// router.use(studyRouter);
-// router.use(teachRouter);
+router.use(studyRouter);
+router.use(teachRouter);
 router.use(facultyRouter);
 // router.use(fsrRouter);
 router.use(adminWorkRouter);

ae39f028a784f867962035281745904afec0a5c1
JomaUrbano <muurbano@up.edu.ph>
2018-05-14 23:07:02 +0800

    Fixed Bugs

--- src/entities/facultygrant/controller.js
+++ src/entities/facultygrant/controller.js
@@ -76,13 +76,13 @@ export const getFacultyGrant = ({ id }) => {
 };
 
 // gets a faculty grant by id by emp_id
-export const getAllFacultyGrantByEmp = ({ emp_id }) => {
+export const getAllFacultyGrantByEmp = ({ id }) => {
   return new Promise((resolve, reject) => {
     const queryString = SqlString.format(
       `
           CALL view_faculty_grant_by_emp_id(?)
         `,
-      [emp_id]
+      [id]
     );
 
     db.query(queryString, (err, rows) => {
